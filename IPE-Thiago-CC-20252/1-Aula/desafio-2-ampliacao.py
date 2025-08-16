09aea40d76fe856dffe2381867d5657960a45216
Thiago Raniery Brito <thiagobritotrs@gmail.com>
2025-08-16 10:53:10 -0300

    exercises-study-guide-1.py

--- IPE-Thiago-CC-20252/1-Aula/desafio-2-ampliacao.py
+++ IPE-Thiago-CC-20252/1-Aula/desafio-2-ampliacao.py
@@ -3,5 +3,6 @@ age = int(input('Digite sua idade: '))
 favColor = input('Digite sua cor favorita: ')
 
 print(f'Que legal, {name}! Então você tem {age} anos e adora a cor {favColor}.')
-print(f'\nQue legal, {name.capitalize()}!\n\nEntão você tem:')
+print(f'\nQue legal, {name.capitalize()}!', end=' *-* ')
+print('Então você tem:')
 print(f'\t-> {age} anos', f'e adora a cor "{favColor}"', sep=' - ')
