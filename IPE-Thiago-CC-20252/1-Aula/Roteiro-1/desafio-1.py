fcf72bc5a970c7bc93e64024c0b31bec324d4cf1
Thiago Raniery Brito <thiagobritotrs@gmail.com>
2025-08-16 10:57:57 -0300

    first class execises

---
+++ IPE-Thiago-CC-20252/1-Aula/Roteiro-1/desafio-1.py
@@ -0,0 +1,13 @@
+name  = 'Thiago'
+age = 22
+city = 'Sumaré'
+profession = 'Manager'
+
+print(f'{name}\n{age}\n{city}\n{profession}')
+print(f'Olá, meu nome é {name}, tenho {age} anos, moro em {city} e sou {profession}.')
+print(f'{age:03d}')
+
+height = 1.722222
+
+print('Altura {0:.2f}'.format(height))
+print(name, city, sep='-')
